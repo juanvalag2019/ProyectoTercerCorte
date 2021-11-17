bdfabc21aa63d7ea3d996a8c2cee75ff66507c3d
juanvalag2019 <juanvalag@unisabana.edu.co>
2021-11-17 16:46:41 -0500

    se añadio ganaste

--- Assets/Scripts/EnemyCounter.cs
+++ Assets/Scripts/EnemyCounter.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class EnemyCounter : MonoBehaviour
 {
     [SerializeField] Text counter;
+    [SerializeField] WonGame wonGame;
 
     // Update is called once per frame
     void Update()
     {
-        counter.text = GameObject.FindGameObjectsWithTag("Enemy").Length.ToString();
+        int enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        counter.text = enemyCount.ToString();
+        if (enemyCount == 0)
+        {
+            Hide();
+            wonGame.SetUp();
+        }
     }
 
     public void Hide()
